783349111515a0f925a8c5cc600b1725baa57de3
37062 <vasiliauskaiteinga@gmail.com>
2023-03-06 20:59:11 +0200

    new Calculator class created

---
+++ src/Calculator.java
@@ -0,0 +1,7 @@
+public class Calculator {
+    //psvm (public static void main line appears) - not so magic shortcut
+    public static void main(String[] args) {
+        System.out.println("Calculator...");
+
+    }
+}
