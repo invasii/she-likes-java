13bf3c376bb28b94113a7f822ae017a316da4699
37062 <vasiliauskaiteinga@gmail.com>
2023-03-09 19:32:19 +0200

    Homework BouncerInDisco class added

---
+++ src/com/sda/she_likes_java/homework/BouncerInDisco.java
@@ -0,0 +1,34 @@
+package com.sda.she_likes_java.homework;
+
+import java.util.Scanner;
+
+public class BouncerInDisco {
+    public static void main(String[] args) {
+        System.out.println("\nBouncer In Disco");
+
+        Scanner inputReader = new Scanner(System.in);
+        System.out.println("\nTell me how old are you?");
+        int personAge = inputReader.nextInt();
+
+        if (personAge >= 16) {
+            System.out.println("That is nice. Well, lets see if you tall enough");
+        } else {
+            System.out.println("You way too young, we need to check you height as well");
+        }
+
+        System.out.println("\n\nTell me what is your height?");
+        Float personHeight = inputReader.nextFloat();
+
+        if (personHeight >= 1.60) {
+            System.out.println("Welcome to the disco!");
+        } else {
+            System.out.println("See you next time little creature!");
+        }
+
+
+
+
+
+
+    }
+}
