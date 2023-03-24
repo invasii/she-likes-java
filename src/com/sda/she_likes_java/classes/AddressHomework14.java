da7f873b9255144c0e71ef82b8195e1fde5016c5
37062 <vasiliauskaiteinga@gmail.com>
2023-03-24 19:12:18 +0200

    homework - amended code

--- src/com/sda/she_likes_java/classes/AddressHomework14.java
+++ src/com/sda/she_likes_java/classes/AddressHomework14.java
@@ -4,29 +4,23 @@ public class AddressHomework14 {
     public static void main(String[] args) {
         System.out.println("My address information using classes");
 
-        Address one = new Address();
-        one.country = "Lithuania";
-        Address two = new Address();
-        two.city = "Kaunas";
-        Address three = new Address();
-        three.street = "Virbeliskiu st.";
-        Address four = new Address();
-        four.postalCode = 11111;
+        Address addressHome  = new Address();
+        addressHome.country = "Lithuania";
+        addressHome.city = "Kaunas";
+        addressHome.street = "Virbeliskiu st.";
+        addressHome.postalCode = 11111;
 
-        System.out.println("My current address is: \n"+ one.country + ", \n" + two.city + ", \n" + three.street + ",\n" + four.postalCode);
+        System.out.println("My current address is: \n"+ addressHome.country + ", \n" + addressHome.city + ", \n" + addressHome.street + ",\n" + addressHome.postalCode);
 
 
         System.out.println("\nMy work address using classes");
-        Address five = new Address();
-        five.country = "Belgium";
-        Address six = new Address();
-        six.city = "Antwerp";
-        Address seven = new Address();
-        seven.street = "Unkown ave.";
-        Address eight = new Address();
-        eight.postalCode = 55555;
+        Address workAddress = new Address();
+        workAddress.country = "Belgium";
+        workAddress.city = "Antwerp";
+        workAddress.street = "Unkown ave.";
+        workAddress.postalCode = 55555;
 
-        System.out.println(String.format("My  work address is: %s, %s, %s, %d. ",five.country, six.city, seven.street, eight.postalCode));
+        System.out.println(String.format("My  work address is: %s, %s, %s, %d. ",workAddress.country, workAddress.city, workAddress.street, workAddress.postalCode));
 
     }
 }
