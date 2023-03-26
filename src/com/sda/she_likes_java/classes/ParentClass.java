53e4efec2b2d119d899040db3478c5e699bcb3e3
37062 <vasiliauskaiteinga@gmail.com>
2023-03-26 19:10:02 +0300

    new class - parent class created

---
+++ src/com/sda/she_likes_java/classes/ParentClass.java
@@ -0,0 +1,10 @@
+package com.sda.she_likes_java.classes;
+
+public class ParentClass {
+    String firstName;
+    String surname;
+    Address address;
+    Person firstChild;
+    Person secondChild;
+
+}
