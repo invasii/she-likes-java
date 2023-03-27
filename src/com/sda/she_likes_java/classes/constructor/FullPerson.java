1e3c9b607563852ee171449b7e99610fce240cc4
37062 <vasiliauskaiteinga@gmail.com>
2023-03-27 21:11:08 +0300

    static method first example

---
+++ src/com/sda/she_likes_java/classes/constructor/FullPerson.java
@@ -0,0 +1,52 @@
+package com.sda.she_likes_java.classes.constructor;
+
+import com.sda.she_likes_java.classes.Address;
+
+public class FullPerson {
+    String name;
+    String surname;
+    int age;
+
+    FullAddress address;
+
+    public FullPerson() {
+    }
+
+    public FullPerson(String name, String surname, int age, FullAddress address) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "FullPerson{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                ", address=" + address +
+                '}';
+    }
+
+
+    public void describeYourself() {
+        System.out.println("Hi I'm " + name);
+    }
+
+    public static void genericDescription() {
+        System.out.println("Generic description");
+    }
+    public static void main(String[] args) {
+        FullAddress address = new FullAddress("Italy", "Roma", "La Romania", "007");
+        FullPerson person = new FullPerson("Inga", "Vasi", 30, address);
+        System.out.println("Person is: " + person);
+
+        person.describeYourself();
+
+        FullPerson personWithoutAddress = new FullPerson("Jonas", "Jonaitis", 25, null);
+        personWithoutAddress.describeYourself();
+
+        genericDescription();
+    }
+}
