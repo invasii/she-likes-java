4b8e41c96ebb46113527374089cdfc0305c4d967
37062 <vasiliauskaiteinga@gmail.com>
2023-03-31 20:14:35 +0300

    another house example with shared house object

--- src/com/sda/she_likes_java/objects/HouseExample.java
+++ src/com/sda/she_likes_java/objects/HouseExample.java
@@ -12,5 +12,20 @@ public class HouseExample {
 
         System.out.println("I've got more rooms now.");
         System.out.println("Currently my house has got: " + myHouse.getNumberOfRooms() + " rooms");
+
+        System.out.println("========================================================");
+        HouseOwner wife = new HouseOwner("Mary", myHouse);
+        HouseOwner husband = new HouseOwner("Jack", myHouse);
+
+        System.out.println("Mary would like to repair her house");
+        wife.getHouse().setNumberOfBathrooms(3); // it is the same as House house = wife.getHouse(); house.getNumberOfBathrooms(3);
+        System.out.println("Currently the house looks like this: " + myHouse);
+
+        husband.getHouse().setNumberOfBathrooms(1);
+        System.out.println("Currently the house looks like this: " + myHouse);
+
+        System.out.println("Mary: " + wife);
+        System.out.println("Jack: " + husband);
+
     }
 }
