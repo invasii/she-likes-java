a20cd74cc11f53d8a03ef02a80cc162752605c07
37062 <vasiliauskaiteinga@gmail.com>
2023-04-24 19:26:21 +0300

    Man and woman classes created

---
+++ src/com/sda/she_likes_java/virtual_methods/Man.java
@@ -0,0 +1,12 @@
+package com.sda.she_likes_java.virtual_methods;
+
+public class Man extends Human {
+    @Override
+    public void whoYouAre() {
+        System.out.println("I'm a man :}");
+    }
+
+    public void learnToClean() {
+        System.out.println("I like it");
+    }
+}
