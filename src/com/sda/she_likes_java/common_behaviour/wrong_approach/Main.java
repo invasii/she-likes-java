071dc51f17f861201fd17caf187b5439592fea84
37062 <vasiliauskaiteinga@gmail.com>
2023-04-26 21:29:04 +0300

    moveable interface introduced

--- src/com/sda/she_likes_java/common_behaviour/wrong_approach/Main.java
+++ src/com/sda/she_likes_java/common_behaviour/wrong_approach/Main.java
@@ -13,5 +13,26 @@ public class Main {
 
         Flyable superman = new Superman();
         Flyable flyableParrot = new Parrot();
+
+        Swimmable tiger2 = (Swimmable) tiger; // these two lines means the same
+        Swimmable swimmableTiger = new Tiger(); //these two lines means the same
+
+        Superman superSuperman = new Superman();
+        Flyable superFly = superSuperman;
+        Swimmable superSwim = superSuperman;
+
+        //Tiger assignment ways
+        Tiger newTiger = new Tiger();
+        newTiger.go();
+        newTiger.swim();
+
+        Animal animalTiger = new Tiger();
+        animalTiger.go();
+
+        Swimmable swimmableTiger2 = new Tiger();
+        swimmableTiger2.swim();
+
+        Object objectTiger = new Tiger();
+        // cannot call go() and swim() they are not visible here (in tiger object)
     }
 }
