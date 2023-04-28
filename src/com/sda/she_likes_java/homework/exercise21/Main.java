6c6e8540475de86b227d4f3a8b12d8de6e084508
37062 <vasiliauskaiteinga@gmail.com>
2023-04-28 19:40:17 +0300

    homework exercise 21 - updated

--- src/com/sda/she_likes_java/homework/exercise21/Main.java
+++ src/com/sda/she_likes_java/homework/exercise21/Main.java
@@ -5,13 +5,17 @@ package com.sda.she_likes_java.homework.exercise21;
 public class Main {
     public static void main(String[] args) {
 
-        Vehicle airplane = new Airplane();
+        Vehicle airplane = new Airplane(); // this is called casting = new Airplane (what is in ship or in higher hierarchy - vehicle in this case)
+       // every class is from object and object is the highest hierarchy
+        airplane.move();
         ((Airplane) airplane).fly();
 
         Vehicle ship = new Ship();
+        ship.move();
         ((Ship) ship).floats();
 
         Vehicle submarine = new Submarine();
+        submarine.move();
         ((Submarine) submarine).dive();
     }
 
--- src/com/sda/she_likes_java/homework/exercise21/Vehicle.java
+++ src/com/sda/she_likes_java/homework/exercise21/Vehicle.java
@@ -2,7 +2,7 @@ package com.sda.she_likes_java.homework.exercise21;
 
 public class Vehicle {
     public void move() {
-        System.out.println("move() means the car is moving");
+        System.out.println("move() means the machine is moving");
     }
-    
+
 }
